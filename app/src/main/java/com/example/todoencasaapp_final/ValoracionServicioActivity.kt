606833cc402920481747641f1cfea3599aa4fcf1
Jesus Zabaleta <jesus.zabaleta@udea.edu.co>
2019-10-15 14:14:29 -0500

    Commit

--- app/src/main/java/com/example/todoencasaapp_final/ValoracionServicioActivity.kt
+++ app/src/main/java/com/example/todoencasaapp_final/ValoracionServicioActivity.kt
@@ -48,6 +48,9 @@ class ValoracionServicioActivity : AppCompatActivity() {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 Log.d("Datos: ",dataSnapshot.value.toString())
                 if(dataSnapshot.exists()){
+                    val nombres = dataSnapshot.child("Tecnico").child(identificacion).child("nombres").value.toString()
+                    val apellidos = dataSnapshot.child("Tecnico").child(identificacion).child("apellidos").value.toString()
+                    nombre_tecnico.text = nombres + " " + apellidos
                     servicio_solicitud.text = "Servicio: " + dataSnapshot.child("DatosServicio").child(identificacion).child("servicio").value.toString()
                     dia_solicitud.text = "Dia: " + dataSnapshot.child("DatosServicio").child(identificacion).child("dia").value.toString()
                     hora_solicitud.text = "Hora: " + dataSnapshot.child("DatosServicio").child(identificacion).child("hora").value.toString()
